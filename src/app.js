4509b471e54fc4658fdeeb32e06f9983d6806f8d
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-05-01 10:03:57 -0400

    adding src and public directories

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const cards = (state, action) => {
+    switch(action.type){
+        case 'ADD_CARD':
+        let newCard = Object.assign({}, action.data, {
+            score: 1,
+            id: +new Date
+        })
+
+        return state.concat([newCard]);
+
+        default: 
+            return state || [];
+    }
+}
+
+// const store = Redux.createStore(function(state, action){
+//     return {
+//         cards: cards(state.cards, action)
+//     }
+// });
+
+const store = Redux.createStore(Redux.combineReducers({
+    cards
+}));
+
+store.subscribe(() => {
+    console.log(store.getState());
+}); 
+
+store.dispatch({
+    type: 'ADD_CARD',
+    data: {
+        front: 'front',
+        back: 'back'
+    }
+})
+store.dispatch({
+    type: 'ADD_CARD',
+    data: {
+        front: 'front',
+        back: 'back'
+    }
+})
